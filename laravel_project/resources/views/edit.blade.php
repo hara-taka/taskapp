3d6a82426f3448b67b59ca32fbe4d90f5c9ed154
harada <aya33798@gmail.com>
2020-03-25 17:43:42 +0900

    task作成中

---
+++ laravel_project/resources/views/edit.blade.php
@@ -0,0 +1,10 @@
+<div class="editTask_header">
+  <h1>タスク編集</h1>
+    <div class="taskEditWrapper">
+      <form action='{{ route('tasks.update', ['user_id' => $user_id]) }}' method="post">
+        {{csrf_field()}}
+          <input type="text" name="name" value="{{ $task -> name }}">
+          <input type="submit" value="更新" class="btn">
+      </form>
+    </<div>
+</div>
