2d1bf0a09ee9ba903aa5484709c91595808612db
harada <aya33798@gmail.com>
2020-03-27 21:40:00 +0900

    タスク達成率作成

--- laravel_project/resources/views/task/edit.blade.php
+++ laravel_project/resources/views/task/edit.blade.php
@@ -4,6 +4,10 @@
       <form action="{{ route('tasks.update', ['user_id' => $user_id,'task_id' => $task->id]) }}" method="post">
         {{csrf_field()}}
           <input type="text" name="name" value="{{ $task -> name }}">
+          <select name="status">
+            <option value="1" {{ $task->status == 1 ? 'selected' : '' }}>未完了</option>
+            <option value="2" {{ $task->status == 2 ? 'selected' : '' }}>完了</option>
+          </select>
           <input type="submit" value="更新" class="btn">
       </form>
     </<div>
--- laravel_project/resources/views/task/index.blade.php
+++ laravel_project/resources/views/task/index.blade.php
@@ -2,17 +2,22 @@
   <div class="personlTask">
     <div class="personalTask_header">
       <h1>タスク</h1>
-      <h2>達成率％</h2>
+      <h2>達成率{{$achievment_rate}}％</h2>
         <div class="taskcreate">
           <form action="{{ route('tasks.store', ['user_id' => $user_id]) }}" method="post">
           {{csrf_field()}}
             <input type="text" name="name">
+            <select name="status">
+              <option value="1">未完了</option>
+              <option value="2">完了</option>
+            </select>
             <input type="submit" value="追加" class="btn">
           </form>
           <div class="taskIndex_Wrapper">
             <table>
               @foreach($tasks as $task)
                 <tr>
+                  <td>{{$task->task_status}}</td>
                   <td> {{$task->name}}</td>
                   <td>
                   <a href="{{ route('tasks.edit', ['user_id' => $user_id,'task_id' => $task->id ]) }}">編集</a>
