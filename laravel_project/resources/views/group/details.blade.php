cd54172874eb39d9f5f933400dca9c8609a581b4
harada <aya33798@gmail.com>
2020-04-01 18:46:26 +0900

    グループ参加人数処理_view修正

--- laravel_project/resources/views/group/details.blade.php
+++ laravel_project/resources/views/group/details.blade.php
@@ -7,9 +7,9 @@
   <h1>{{$group->comment}}</h1>
   <form action="{{ route('groups.participate', ['group_id' => $group->id]) }}" method="post">
     {{csrf_field()}}
-    @if($group_member_num > 5)
+    @if($group_member_num > 4)
       <h1>定員に達したため参加できません</h1>
     @endif
-    <input type="submit" value="このグループに参加する" {{ $group_member_num > 5 ? 'disabled="disabled"' : '' }}>
+    <input type="submit" value="このグループに参加する" {{ $group_member_num > 4 ? 'disabled="disabled"' : '' }}>
   </form>
 </div>
